7f4ba99f626cd2a34d61077dbdc577f4a6cdefb4
Bionca Bond <bioncab@zillowgroup.com>
2022-02-09 21:00:46 -0800

    all test passing

--- cryptography/encrypt.py
+++ cryptography/encrypt.py
@@ -1,6 +1,74 @@
 import nltk 
+# nltk.download_shell()  
 
+words_list= nltk.corpus.words.words()
 
-# Comment
+def encrypt(msg,key): 
+    encrypted_msg = "" 
+    msg = msg.lower()
+    key = key % 26  
 
-# nltk.download_shell() 
+    for char in str(msg): 
+        if ord(char) not in range(97,123): 
+            encrypted_msg += char 
+
+        else: 
+        #     #this gets us the ascii code and the key 
+            add_char = (ord(char) + key) 
+            # print(f"this is add letter: {add_letter}")
+            # if the letter is outside of our range we want to handle
+            #how to restart the letter loop 
+            if add_char > 122: 
+                #how far are we from the end of the alphabet  
+                letters_end = 122 - ord(char) 
+                # print(f"this is ord(char)/ascii: {ord(char)}")
+
+                move_over = key - letters_end - 1 
+                # print(f"move_over: key {key}, letters_end: {letters_end}")
+
+                letter_a_start = 97 + move_over  
+                # print(f"this is letter_a_start: {letter_a_start} ") 
+
+                encrypted_msg += chr(letter_a_start)
+            else:
+                encrypted_msg += chr(ord(char)+ key)
+    print(f"encrypted message final: {encrypted_msg}")
+
+    return encrypted_msg
+
+def decrypt(msg,key): 
+    return encrypt(msg,-key) 
+
+def real_words(msg): 
+    num_of_words = 0
+    words = msg.split(" ")
+   
+    for word in words:
+        if word in words_list:
+            num_of_words += 1
+    if num_of_words >= 0:
+        print(f"num_of_words: {num_of_words}") 
+        return num_of_words 
+
+def crack(msg): 
+    msg = msg.lower() 
+    sentence = ""
+    for key in range(26): 
+        decrypt_msg = decrypt(msg, key) 
+        num_of_words = real_words(decrypt_msg) 
+
+        if num_of_words >= 1 :
+            sentence = decrypt_msg 
+            print(f"crack num_of_words: {num_of_words}") 
+    print(f"sentence: {sentence}")
+    return sentence
+
+if __name__== "__main__": 
+    words_list = nltk.corpus.words.words() 
+
+    # msg = encrypt("hell0 w0rld$",3)
+    msg = encrypt("It was the best of times, it was the worst of times.",3)
+    print(f"encrypt: {msg}") 
+    # real_words_msg = real_words("hello world blue") 
+    # crack_msg = crack("hello world crack")
+   
--- tests/test_cryptography.py
+++ tests/test_cryptography.py
@@ -1,5 +1,35 @@
+import pytest
+from cryptography.encrypt import *
+
 from cryptography import __version__
 
-
+# Mic Check 
 def test_version():
     assert __version__ == '0.1.0'
+
+#Encrypt 
+def test_encrypt_a_string(): 
+    e = encrypt("HELLO World",3)
+    actual = e
+    expected = "khoor zruog" 
+    assert actual == expected
+
+#outside of our char range 
+def test_char_out_of_range(): 
+    e = encrypt("Hell0 W0rld$",3)
+    actual = e
+    expected = "khoo0 z0uog$" 
+    assert actual == expected
+
+#decrypt 
+def test_decrypt_a_string(): 
+    msg = ("khoo0 z0uog$")
+    actual = decrypt(msg,3)
+    expected = "hell0 w0rld$" 
+    assert actual == expected 
+
+#crack 
+def test_crack_the_code(): 
+    msg = "lw zdv wkh ehvw ri wlphv, lw zdv wkh zruvw ri wlphv."
+    actual = crack(msg) 
+    expected = "It was the best of times, it was the worst of times."
